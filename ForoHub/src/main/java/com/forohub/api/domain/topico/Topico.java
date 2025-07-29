76ae46f3f1822312bf9eea2623a17db5140cb889
Luciano-AOSC <luciano.gatti.one@gmail.com>
2025-07-29 13:01:29 -0300

    Errores y autenticacion
    Creacion de las clases para la autenticacion y tratamiento de errores

---
+++ ForoHub/src/main/java/com/forohub/api/domain/topico/Topico.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.forohub.api.domain.topico;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensaje;
+    private LocalDateTime fechaDeCreacion;
+    private String status;
+    private Usuario autor;
+    private Curso curso;
+    private List<Respuesta> respuestas;
+}
