a25fb735c4e12fbdf9b9329df8a8a3e9496c1d17
Luciano-AOSC <luciano.gatti.one@gmail.com>
2025-07-31 11:53:03 -0300

    Update DatosCreacionTopico.java
    Agregacion de imports

--- ForoHub/src/main/java/com/forohub/api/domain/topico/DatosCreacionTopico.java
+++ ForoHub/src/main/java/com/forohub/api/domain/topico/DatosCreacionTopico.java
@@ -1,9 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
- */
-
 package com.forohub.api.domain.topico;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 /**
  *
