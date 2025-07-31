c07ad05d0407c5957ffe41b5f3cea785498d7bf2
Luciano-AOSC <luciano.gatti.one@gmail.com>
2025-07-31 11:52:05 -0300

    Update Perfil.java

--- ForoHub/src/main/java/com/forohub/api/domain/perfil/Perfil.java
+++ ForoHub/src/main/java/com/forohub/api/domain/perfil/Perfil.java
@@ -2,9 +2,10 @@ package com.forohub.api.domain.perfil;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-
+import com.forohub.api.domain.usuarios.Usuario;
 
 /**
 * Representa un perfil dentro del sistema.
@@ -16,6 +17,7 @@ import lombok.AllArgsConstructor;
 @Entity(name = "Perfil")
 @Table(name = "perfiles")
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Perfil {
@@ -23,4 +25,7 @@ public class Perfil {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
+    
+    @ManyToOne
+    private Usuario usuario;
 }
