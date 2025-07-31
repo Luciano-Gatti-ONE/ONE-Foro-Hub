4912b9eed18502ddc271c6ac035d4cc58ec5d4f9
Luciano-AOSC <luciano.gatti.one@gmail.com>
2025-07-31 11:53:10 -0300

    Update DatosRespuestaTopico.java
    Agregacion de imports

--- ForoHub/src/main/java/com/forohub/api/domain/topico/DatosRespuestaTopico.java
+++ ForoHub/src/main/java/com/forohub/api/domain/topico/DatosRespuestaTopico.java
@@ -1,9 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
- */
-
 package com.forohub.api.domain.topico;
+
+import java.time.LocalDateTime;
 
 /**
  *
@@ -15,7 +12,7 @@ public record DatosRespuestaTopico(Long id, String mensaje, String titulo,
 {
     public DatosRespuestaTopico(Topico topico) {
         this(topico.getId(), topico.getMensaje(), topico.getTitulo(), 
-                topico.getFechaCreacion(), topico.getStatus().getDescripcion(), 
+                topico.getFechaDeCreacion(), topico.getStatus().getDescripcion(), 
                 topico.getAutor().getNombre(), topico.getCurso().getNombre()
             );
     }
