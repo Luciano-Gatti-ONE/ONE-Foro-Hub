7f4c396b793eba177545bb6589fbd828ba18c810
Luciano-AOSC <luciano.gatti.one@gmail.com>
2025-08-06 09:23:14 -0300

    Update DatosCreacionRespuesta.java

--- ForoHub/src/main/java/com/forohub/api/domain/respuesta/DatosCreacionRespuesta.java
+++ ForoHub/src/main/java/com/forohub/api/domain/respuesta/DatosCreacionRespuesta.java
@@ -1,12 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Record.java to edit this template
- */
-
 package com.forohub.api.domain.respuesta;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 /**
  *
  * @author usuario
  */
-public record DatosCreacionRespuesta(String mensaje) {}
+public record DatosCreacionRespuesta(
+        @NotNull Long idAutor,
+        @NotNull Long idTopico,
+        @NotBlank String mensaje
+) {}
