c411130492963c6486d32850d6eb36fbb4c12ea4
Luciano-AOSC <luciano.gatti.one@gmail.com>
2025-08-06 09:23:29 -0300

    Update RespuestaService.java

--- ForoHub/src/main/java/com/forohub/api/domain/respuesta/RespuestaService.java
+++ ForoHub/src/main/java/com/forohub/api/domain/respuesta/RespuestaService.java
@@ -1,6 +1,24 @@
 package com.forohub.api.domain.respuesta;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.forohub.api.domain.topico.TopicoRepository;
+import com.forohub.api.domain.respuesta.RespuestaRepository;
+import com.forohub.api.domain.usuarios.UsuarioRepository;
+import com.forohub.api.domain.ValidacionException;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+import com.forohub.api.domain.respuesta.DatosActualizarRespuesta;
+import com.forohub.api.domain.respuesta.DatosCreacionRespuesta;
+import com.forohub.api.domain.respuesta.DatosDetalleRespuesta;
+import com.forohub.api.domain.respuesta.DatosDetalleRespuestaActualizada;
+import com.forohub.api.domain.respuesta.DatosMostrarRespuesta;
+import com.forohub.api.domain.respuesta.Respuesta;
 
 /**
  *
@@ -24,78 +42,53 @@ public class RespuestaService {
                  .orElseThrow(() -> new ValidacionException("El usuario no existe o se encuentra inactivo"));
         var topico = topicoRepository.findById(datos.idTopico())
                  .orElseThrow(() -> new ValidacionException("No existe un topico con el id informado"));
-        var respuesta = new Respuesta(datos, autorRespuesta, topico);
+        var respuesta = new Respuesta(datos.mensaje(), autorRespuesta, topico);
         respuestaRepository.save(respuesta);
         return new DatosDetalleRespuesta(respuesta);
     }
     
-    public List<DatosMostrarRespuestas> listarRespuestasPorTopico(Long id){
-        var respuestas = respuestaRepository.findAllByTopicoId(id)
+    public List<DatosMostrarRespuesta> listarRespuestasPorTopico(Long id){
+        var respuestas = respuestaRepository.findByActivoTrueAndTopicoId(id)
                 .stream()
-                .map(DatosMostrarRespuestas::new)
+                .map(DatosMostrarRespuesta::new)
                 .collect(Collectors.toList());
         return respuestas;
     }
     
-    public DatosMostrarTopico mostrarTopicoPorId(Long id){
-        var topico = topicoRepository.findById(id)
-                .orElseThrow(() -> new ValidacionException("No existe un usuario con el id informado"));
-        var respuesta = new DatosMostrarTopico(topico);
-        return respuesta;
+    public DatosMostrarRespuesta respuestaPorId(Long id){
+        var respuestaEntity = respuestaRepository.findById(id)
+                .orElseThrow(() -> new ValidacionException("No existe una respuesta con el id informado"));
+        var datosRespuesta = new DatosMostrarRespuesta(respuestaEntity);
+        return datosRespuesta;
     }
     
-    public DatosRespuestaTopicoActualizado actualizarTopico(DatosActualizarTopico datos){
-        validaciones.forEach(v -> v.validarActualizacion(datos));
-        var topico = topicoRepository.findById(datos.id())
-            .orElseThrow(() -> new ValidacionException("Tópico no encontrado con id: " + datos.id()));
-        var avisos = actualizarDatos(datos, topico);
-        topicoRepository.save(topico); 
-        return new DatosRespuestaTopicoActualizado(topico, avisos);
+    public DatosDetalleRespuestaActualizada actualizarRespuesta(DatosActualizarRespuesta datos){
+        var respuestaEntity = respuestaRepository.findById(datos.id())
+            .orElseThrow(() -> new ValidacionException("Respuesta no encontrada"));
+        var avisos = actualizarDatos(datos, respuestaEntity);
+        respuestaRepository.save(respuestaEntity); 
+        return new DatosDetalleRespuestaActualizada(respuestaEntity, avisos);
     }
     
-    public void eliminarTopico(Long id){
-        topicoRepository.deleteById(id);       
+    public void desactivarRespuesta(Long id){
+        var respuesta = respuestaRepository.getReferenceById(id);      
+        respuesta.desactivarRespuesta();    
     }
     
-    public List<DatosRespuestaTopico> topicosPorFecha(){
-        var topicos = topicoRepository.findTop10ByOrderByFechaDeCreacionAsc();
-        var respuesta = topicos.stream()
-            .map(DatosRespuestaTopico::new)
-            .toList();
-        return respuesta;
-    }
-    
-
-    public List<DatosRespuestaTopico> topicosPorCursoyAño(DatosBusquedaTopico datosBusquedaTopico){
-        var topicos = topicoRepository.buscarTopicosPorCursoYAño(
-                datosBusquedaTopico.nombreCurso(), 
-                datosBusquedaTopico.añoTopico()
-        );
-        var respuesta = topicos.stream()
-                .map(DatosRespuestaTopico::new)
-                .collect(Collectors.toList());
-        return respuesta;
+    public void marcarComoSolucion(Long id){
+        var respuesta = respuestaRepository.getReferenceById(id); 
+        respuesta.marcarSolucion();
     }
     
     public Map<String, String> actualizarDatos(DatosActualizarRespuesta datosActualizarRespuesta, Respuesta respuesta) {
         Map<String, String> avisos = new HashMap<>();
 
-        if (datosActualizarTopico.titulo() != null && !datosActualizarTopico.titulo().isBlank()) {
-            topico.setTitulo(datosActualizarTopico.titulo());
-        } else if (datosActualizarTopico.titulo() != null) {
-            avisos.put("Titulo", "El título no fue actualizado porque está vacío o en blanco.");
+        if (datosActualizarRespuesta.mensaje() != null && !datosActualizarRespuesta.mensaje().isBlank()) {
+            respuesta.setMensaje(datosActualizarRespuesta.mensaje());
+        } else {
+            avisos.put("Mensaje", "El mensaje se encuentra vacio");
         }
-
-        if (datosActualizarTopico.mensaje() != null && !datosActualizarTopico.mensaje().isBlank()) {
-            topico.setMensaje(datosActualizarTopico.mensaje());
-        } else if (datosActualizarTopico.mensaje() != null) {
-            avisos.put("Mensaje", "El mensaje no fue actualizado porque está vacío o en blanco.");
-        }
+        
         return avisos;
-    }
-     
-    public void desactivarRespuesta(Long id){
-        var respuesta = respuestaRepository.getReferenceById(id);      
-        respuesta.desactivarRespuesta();    
     }
 }
