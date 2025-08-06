27a57bf4a73c2da724c63f5c9dc2150b1d649368
Luciano-AOSC <luciano.gatti.one@gmail.com>
2025-08-06 09:23:26 -0300

    Update RespuestaRepository.java

--- ForoHub/src/main/java/com/forohub/api/domain/respuesta/RespuestaRepository.java
+++ ForoHub/src/main/java/com/forohub/api/domain/respuesta/RespuestaRepository.java
@@ -2,10 +2,12 @@ package com.forohub.api.domain.respuesta;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.forohub.api.domain.respuesta.Respuesta;
+import java.util.List;
 
 /**
  *
  * @author usuario
  */
 public interface RespuestaRepository extends JpaRepository<Respuesta, Long> {
+    List<Respuesta> findByActivoTrueAndTopicoId(Long id);
 }
